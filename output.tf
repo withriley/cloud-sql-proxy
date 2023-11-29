d08b1286d4bff1e1a133172c2946e72f3f7bf952
Sam Bentley <sam.bentley@withriley.com>
2023-11-29 16:18:53 +1100

    add test output

--- output.tf
+++ output.tf
@@ -6,3 +6,7 @@ output "tunnel_cmd" {
     description = "The command to run to connect to the Cloud SQL instance via IAP"
     value       = "gcloud compute start-iap-tunnel ${google_compute_instance.default.name} ${data.google_sql_database_instance.default.settings[0].ip_configuration[0].ipv4_enabled[0].port} --local-host-port=localhost:${data.google_sql_database_instance.default.settings[0].ip_configuration[0].ipv4_enabled[0].port} --zone=${google_compute_instance.default.zone}"
 }
+output "db_instance" {
+    description = "The Cloud SQL instance name"
+    value       = data.google_sql_database_instance.default
+}
